0239b550626dc9317690d073d40669021695f43f
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-06 15:46:17 +0700

    Update script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 conda create -n lsnet python==3.8
 conda activate lsnet
 pip install -r requirements.txt
+
+curl -L -o dataset/vnfood-30-100.zip \
+    https://www.kaggle.com/api/v1/datasets/download/meowluvmatcha/vnfood-30-100
+
+unzip dataset/vnfood-30-100.zip
