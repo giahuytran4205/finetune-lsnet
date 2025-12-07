234f2a3cfc8e20e20bcc035696cfede534b88120
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-07 23:29:13 +0700

    update

--- detection/configs/retinanet_lsnet_t_fpn_1x_food_coco.py
+++ detection/configs/retinanet_lsnet_t_fpn_1x_food_coco.py
@@ -1,39 +1,66 @@
-_base_ = [
-    './_base_/models/retinanet_lsnet_fpn.py',
-    './_base_/datasets/foodpixcomplete_detection.py',
-    './_base_/schedules/schedule_1x.py', 
-    './_base_/default_runtime.py'
-]
+_base_ = './retinanet_lsnet_t_fpn_1x_coco.py'
+
+dataset_type = 'CocoDataset'
+data_root = 'dataset/UECFOODPIXCOMPLETE_COCO/'
 
 model = dict(
-    pretrained=None,
     backbone=dict(        
         type='lsnet_t',
-        pretrained="pretrain/lsnet_t.pth",
+        pretrained="pretrain/lsnet_t.pth", 
         frozen_stages=-1,
-        ),
+    ),
     neck=dict(
         type='LSNetFPN',
         in_channels=[64, 128, 256, 384],
         out_channels=256,
         start_level=0,
         num_outs=5,
-        ))
+    ),
+    bbox_head=dict(
+        type='RetinaHead',
+        num_classes=102,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            octave_base_scale=3, 
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64] 
+        ),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]
+        ),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0
+        ),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)
+    )
+)
 
-# optimizer
-optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
-                 paramwise_cfg=dict(custom_keys={'attention_biases': dict(decay_mult=0.),
-                                                 'attention_bias_idxs': dict(decay_mult=0.),
-                                                 }))
-# optimizer_config = dict(grad_clip=None)
-# do not use mmdet version fp16
-# fp16 = None
-optimizer_config = dict(grad_clip=None)
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[8, 11])
-total_epochs = 12
+# Modify dataset related settings
+classes = ('rice', 'eels on rice', 'pilaf', "chicken-'n'-egg on rice", 'pork cutlet on rice', 'beef curry', 'sushi', 'chicken rice', 'fried rice', 'tempura bowl', 'bibimbap', 'toast', 'croissant', 'roll bread', 'raisin bread', 'chip butty', 'hamburger', 'pizza', 'sandwiches', 'udon noodle', 'tempura udon', 'soba noodle', 'ramen noodle', 'beef noodle', 'tensin noodle', 'fried noodle', 'spaghetti', 'Japanese-style pancake', 'takoyaki', 'gratin', 'sauteed vegetables', 'croquette', 'grilled eggplant', 'sauteed spinach', 'vegetable tempura', 'miso soup', 'potage', 'sausage', 'oden', 'omelet', 'ganmodoki', 'jiaozi', 'stew', 'teriyaki grilled fish', 'fried fish', 'grilled salmon', 'salmon meuniere', 'sashimi', 'grilled pacific saury', 'sukiyaki', 'sweet and sour pork', 'lightly roasted fish', 'steamed egg hotchpotch', 'tempura', 'fried chicken', 'sirloin cutlet', 'nanbanzuke', 'boiled fish', 'seasoned beef with potatoes', 'hambarg steak', 'beef steak', 'dried fish', 'ginger pork saute', 'spicy chili-flavored tofu', 'yakitori', 'cabbage roll', 'rolled omelet', 'egg sunny-side up', 'fermented soybeans', 'cold tofu', 'egg roll', 'chilled noodle', 'stir-fried beef and peppers', 'simmered pork', 'boiled chicken and vegetables', 'sashimi bowl', 'sushi bowl', 'fish-shaped pancake with bean jam', 'shrimp with chill source', 'roast chicken', 'steamed meat dumpling', 'omelet with fried rice', 'cutlet curry', 'spaghetti meat sauce', 'fried shrimp', 'potato salad', 'green salad', 'macaroni salad', 'Japanese tofu and vegetable chowder', 'pork miso soup', 'chinese soup', 'beef bowl', 'kinpira-style sauteed burdock', 'rice ball', 'pizza toast', 'dipping noodles', 'hot dog', 'french fries', 'mixed rice', 'goya chanpuru', 'others', 'beverage')
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        classes=classes,
+        ann_file=data_root + 'train/annotations.json',
+        img_prefix=data_root),
+    val=dict(
+        classes=classes,
+        ann_file=data_root + 'test/annotations.json',
+        img_prefix=data_root),
+    test=dict(
+        classes=classes,
+        ann_file=data_root + 'test/annotations.json',
+        img_prefix=data_root))
+evaluation = dict(interval=1, metric='bbox')
