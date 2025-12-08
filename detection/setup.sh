5908a3a55de5d304d1f102d59ba677578f79d71d
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-08 14:10:21 +0700

    update

--- detection/setup.sh
+++ detection/setup.sh
@@ -294,10 +294,6 @@ EOF
 
 git clone https://huggingface.co/jameslahm/lsnet ./pretrain
 
-cd pretrain
-curl -O https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth
-cd ..
-
 # 5. Tải data
 echo ">>> Đang tải dataset..."
 mkdir dataset
