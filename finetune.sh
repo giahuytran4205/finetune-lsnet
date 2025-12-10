c7d6e680709ebc5f9cbdb7ff929aa22bbeeabe89
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-10 15:48:08 +0700

    update

--- finetune.sh
+++ finetune.sh
@@ -2,6 +2,7 @@
 
 OPTION=$1
 GPUS=$2
+EPOCHS=$3
 
 if [ -z "$OPTION" ]; then
     echo "Lỗi: Bạn chưa nhập tham số."
@@ -10,13 +11,22 @@ if [ -z "$OPTION" ]; then
 fi
 
 case $OPTION in
-lsnet_t) python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 12345 --use_env main.py --model lsnet_t --data-set VNFood --data-path dataset/vnfood-30-100/vnfood_combined_dataset \
-    --finetune https://huggingface.co/jameslahm/lsnet/resolve/main/lsnet_t.pth --output_dir finetuned/lsnet_t --epochs 30
-;;
-lsnet_s) python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 12345 --use_env main.py --model lsnet_s --data-set VNFood --data-path dataset/vnfood-30-100/vnfood_combined_dataset \
-    --finetune https://huggingface.co/jameslahm/lsnet/resolve/main/lsnet_s.pth --output_dir finetuned/lsnet_s --epochs 30
-;;
-lsnet_b) python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 12345 --use_env main.py --model lsnet_b --data-set VNFood --data-path dataset/vnfood-30-100/vnfood_combined_dataset \
-    --finetune https://huggingface.co/jameslahm/lsnet/resolve/main/lsnet_b.pth --output_dir finetuned/lsnet_b --weight-decay 0.05 --epochs 20
-;;
+lsnet_t)
+    python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 12345 --use_env main.py \
+        --model lsnet_t --data-set VNFood --data-path dataset/vnfood-30-100/vnfood_combined_dataset \
+        --finetune https://huggingface.co/jameslahm/lsnet/resolve/main/lsnet_t.pth \
+        --output_dir finetuned/lsnet_t --epochs $EPOCHS
+    ;;
+lsnet_s)
+    python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 12345 --use_env main.py \
+        --model lsnet_s --data-set VNFood --data-path dataset/vnfood-30-100/vnfood_combined_dataset \
+        --finetune https://huggingface.co/jameslahm/lsnet/resolve/main/lsnet_s.pth \
+        --output_dir finetuned/lsnet_s --epochs $EPOCHS
+    ;;
+lsnet_b)
+    python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port 12345 --use_env main.py \
+        --model lsnet_b --data-set VNFood --data-path dataset/vnfood-30-100/vnfood_combined_dataset \
+        --finetune https://huggingface.co/jameslahm/lsnet/resolve/main/lsnet_b.pth \
+        --output_dir finetuned/lsnet_b --weight-decay 0.05 --epochs $EPOCHS
+    ;;
 esac
