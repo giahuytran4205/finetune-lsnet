208de9a33e4b9ef4ed3902f5dfb9090d3537804f
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2025-12-10 15:04:03 +0700

    update

--- detection/eval.sh
+++ detection/eval.sh
@@ -3,3 +3,5 @@ bash ./dist_test.sh configs/retinanet_lsnet_t_fpn_1x_coco.py pretrain/lsnet_t_re
 
 # For Mask R-CNN
 bash ./dist_test.sh configs/mask_rcnn_lsnet_t_fpn_1x_coco.py pretrain/lsnet_t_maskrcnn.pth 8 --eval bbox segm --out results.pkl
+
+bash ./dist_test.sh configs/mask_rcnn_lsnet_b_fpn_1x_food_coco.py checkpoints/pretrain/mask_rcnn_lsnet_b_finetuned.pth 1 --eval bbox segm proposal --work-dir ./eval --show --show-dir ./eval
